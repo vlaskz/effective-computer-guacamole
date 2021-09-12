81505a893c06db6298d672ff7859344eba84d245
C1322118 Isaias Cerqueira Velasquez <c1322118@interno.bb.com.br>
2021-09-12 08:32:39 -0300

    Unmarshalling JSON body.

--- chapter1/ex02/reading_writing_json.go
+++ chapter1/ex02/reading_writing_json.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -12,23 +13,39 @@ var i = 0
 
 type helloWorldResponse struct {
 	Message string `json:"message"`
-	Author  string `json:"author"`
-	Date    string `json:", omitempty"`
-	Id      int    `json:"id, string"`
+	Author  string `json:"-"`
+	Date    string `json:",omitempty"`
+	Id      int    `json:"id,string"`
+}
+
+type helloWorldRequest struct {
+	Name string `json:"name"`
 }
 
 func main() {
 
-	http.HandleFunc("/helloworld", helloWorldHandled)
+	http.HandleFunc("/helloworld", helloWorldHandler)
 
 	log.Printf("Server Starting on port %v", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 
 }
 
-func helloWorldHandled(w http.ResponseWriter, r *http.Request) {
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 
-	response := helloWorldResponse{Message: "helloWorld Still the best!", Id: i}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	var request helloWorldRequest
+	err = json.Unmarshal(body, &request)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	response := helloWorldResponse{Message: "You saluted the world, " + request.Name, Id: i}
 	encoder := json.NewEncoder(w)
 	encoder.Encode(&response)
 	i++
